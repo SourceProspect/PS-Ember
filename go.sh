d876c37f3070eda6a32d0e65b12d490fb9d38eae
JoshMcCall <joshmccall221@gmail.com>
2018-04-02 18:55:57 -0500

    go

--- go.sh
+++ go.sh
@@ -1,14 +1,27 @@
-. ~/.nvm/nvm.sh
-nvm i --lts
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install java-common oracle-java8-installer
+#sudo apt-get install oracle-java8-set-default
+#source /etc/profile
+#
+#export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+#export PATH=$JAVA_HOME/bin:$PATH
 
-rm -rf dist docs;
-ember build --environment=production
-mv dist docs;
-git add docs -f;
-git commit -am "prod build";
-git push;
+#sudo add-apt-repository ppa:cwchien/gradle
+#sudo apt-get update
+#sudo apt-get install gradle
+#sudo add-apt-repository ppa:maarten-fonville/android-studio
+#sudo apt-get update
+#sudo apt-get install android-studio
 
-# serve things
-fuser -k 8080/tcp
-http-server ./docs & # run in background for fun
-surge --domain prospectsource.surge.sh ./docs
+#~/Android/Sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-19" "build-tools;19.1.0"
+
+export ANDROID_HOME="/home/crou/Android/"
+echo 'ANDROID_HOME'
+echo $ANDROID_HOME
+whoami
+echo 'ANDROID_HOME'
+export PATH="${ANDROID_HOME}/SDK/tools/:${ANDROID_HOME}tools/:${ANDROID_HOME}/SDK/platform-tools/:${ANDROID_HOME}platform-tools/:${PATH}";
+. ~/.nvm/nvm.sh; nvm use --lts;
+corber platform add android
+corber proxy requirements
